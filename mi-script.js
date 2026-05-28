e2290dda7cb4566a50d729bb64f99fe3cf55b324
Fabián Acosta <acostacidedu@gmail.com>
2026-05-28 14:20:04 -0300

    iniciando repositorio n1

---
+++ mi-script.js
@@ -0,0 +1,38 @@
+console.log("Hola mundo cruel");
+// Con const: el valor no cambia
+const nombreCurso = "Programación Full Stack";
+const anio = 2026;
+
+//Con let:  el valor puede cambiar
+
+let notaEstudiante=7;
+notaEstudiante=9; 
+
+console.log(nombreCurso);
+console.log(notaEstudiante);
+
+//Tipos de Datos
+// String (texto)
+let nombre = "Valentina";
+let saludo = 'Hola mundo';
+
+// Number (número)
+let edad = 17;
+let promedio = 8.5;
+
+// Boolean
+let estaAprobado = true;
+let tieneTarea = false;
+
+// null — ausencia intencional de valor
+let direccion = null;
+
+// undefined — sin valor asignado
+let telefono;
+console.log(telefono); // undefined
+
+// typeof: ver el tipo de una variable
+console.log(typeof nombre);       // "string"
+console.log(typeof edad);         // "number"
+console.log(typeof estaAprobado); // "boolean"
+console.log(direccion);
